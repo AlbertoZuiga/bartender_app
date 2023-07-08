4819bc4592240594934652457009dac2e506cce2
Personal <personal@AirdeAlberto3.home>
2023-07-08 11:16:44 -0400

    CRUD for ingredient ready

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,8 +1,63 @@
 class IngredientsController < ApplicationController
+    before_action :set_ingredient, only: %i[ show edit update destroy]
+
     def index
         @ingredients = Ingredient.all
     end
 
     def show
+    end
+
+    def new
+        @ingredient = Ingredient.new
+    end
+
+    def edit
+    end
+
+    def create
+        @ingredient = Ingredient.new(ingredient_params)
+
+        respond_to do |format|
+            if @ingredient.save
+              format.html { redirect_to ingredient_url(@ingredient), notice: "Ingredient was successfully created." }
+              format.json { render :show, status: :created, location: @ingredient }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+              format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @ingredient.update(ingredient_params)
+              format.html { redirect_to ingredient_url(@ingredient), notice: "Ingredient was successfully created." }
+              format.json { render :show, status: :created, location: @ingredient }
+            else
+              format.html { render :new, status: :unprocessable_entity }
+              format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @ingredient.destroy
+    
+        respond_to do |format|
+          format.html { redirect_to ingredients_url, notice: "Ingredient was successfully destroyed." }
+          format.json { head :no_content }
+        end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ingredient
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def ingredient_params
+      params.require(:ingredient).permit(:name)
     end
 end
